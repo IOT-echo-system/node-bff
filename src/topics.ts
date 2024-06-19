d1bb8eaec39115087813fd2bd2c28ee515305e83
Shivam Rajput <raj.shiviraj@gmail.com>
2024-06-19 10:52:06 +0530

    Shivi | add topic for level monitor.

--- src/topics.ts
+++ src/topics.ts
@@ -5,7 +5,8 @@ type WidgetTopics = { name: string; topics: Array<{ topic: string }> }
 const widget: Record<WidgetType, WidgetTopics> = {
   BOARD: { name: 'BOARD', topics: [] },
   INVOICE: { name: 'INVOICE', topics: [{ topic: 'action/+/code/+' }, { topic: 'action/+' }] },
-  COLLECTION_OF_BUTTONS: { name: 'COLLECTION_OF_BUTTONS', topics: [{ topic: 'update-value' }] }
+  COLLECTION_OF_BUTTONS: { name: 'COLLECTION_OF_BUTTONS', topics: [{ topic: 'update-value' }] },
+  LEVEL_MONITOR: { name: 'LEVEL_MONITOR', topics: [{ topic: 'update-value' }] }
 }
 
 const createTopics = (): string[] => {
