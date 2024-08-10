2da7e9bc87dadac1736a29e92945daf9d39ee127
Shivam Rajput <raj.shiviraj@gmail.com>
2024-08-10 08:29:06 +0530

    Shivi | reading code from mqtt message.

--- src/handler/mqttHandler.ts
+++ src/handler/mqttHandler.ts
@@ -28,7 +28,7 @@ export const handleMqttMessage = (topic: string, payload: Buffer, packet: Packet
         return
       }
       case 'INVOICE': {
-        invoiceService.handle(mqttTopicData as InvoiceData).catch(() => ({}))
+        invoiceService.handle({ ...mqttTopicData, ...JSON.parse(payload.toString('utf-8')) } as InvoiceData).catch(() => ({}))
         return
       }
       case 'COLLECTION_OF_BUTTONS': {
